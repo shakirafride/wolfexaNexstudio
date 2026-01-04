38c758879ffc864a4130487eae9ae13fd24dd358
Shakir <shakir@example.com>
2026-01-04 08:02:48 -0800

    All error solved

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,11 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
-const basePath = isProd ? "/NexaStudio" : "";
-
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath,
-  assetPrefix: basePath,
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
   },
 };
 
